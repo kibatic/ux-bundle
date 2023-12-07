4a46383eec2698827ee7b88f15b766a75b4d1fe7
Jérémy CROMBEZ <777666+jcrombez@users.noreply.github.com>
2023-12-07 15:52:21 +0100

    add asset mapper path

--- src/DependencyInjection/KibaticUXExtension.php
+++ src/DependencyInjection/KibaticUXExtension.php
@@ -4,13 +4,15 @@ namespace Kibatic\UX\DependencyInjection;
 
 use Kibatic\UX\EventListener\ResponseListener;
 use Kibatic\UX\Turbo;
+use Symfony\Component\AssetMapper\AssetMapperInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Twig\Environment;
 
-class KibaticUXExtension extends Extension
+class KibaticUXExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * @throws \Exception
@@ -29,13 +31,33 @@ class KibaticUXExtension extends Extension
                 new Reference('request_stack')
             ])
         ;
+    }
 
-//        $container->prependExtensionConfig('framework', [
-//            'asset_mapper' => [
-//                'paths' => [
-//                    __DIR__.'/../../assets' => '@kibatic/ux',
-//                ],
-//            ],
-//        ]);
+    public function prepend(ContainerBuilder $container): void
+    {
+        if ($this->isAssetMapperAvailable($container)) {
+            $container->prependExtensionConfig('framework', [
+                'asset_mapper' => [
+                    'paths' => [
+                        __DIR__.'/../../assets/dist' => '@kibatic/ux-bundle',
+                    ],
+                ],
+            ]);
+        }
+    }
+
+    private function isAssetMapperAvailable(ContainerBuilder $container): bool
+    {
+        if (!interface_exists(AssetMapperInterface::class)) {
+            return false;
+        }
+
+        // check that FrameworkBundle 6.3 or higher is installed
+        $bundlesMetadata = $container->getParameter('kernel.bundles_metadata');
+        if (!isset($bundlesMetadata['FrameworkBundle'])) {
+            return false;
+        }
+
+        return is_file($bundlesMetadata['FrameworkBundle']['path'].'/Resources/config/asset_mapper.php');
     }
 }
