173fb54b04b14ba9ca7706302c761613d009627f
JCrombez <jeremy.crombez@kibatic.com>
2025-02-11 14:03:50 +0100

    add section around show table tpl

--- templates/maker/crud/templates/show.tpl.php
+++ templates/maker/crud/templates/show.tpl.php
@@ -15,14 +15,16 @@
         {% endif %}
     </header>
 
-    <table class="table table-show">
-        <tbody>
-<?php foreach ($entity_fields as $field): ?>
-            <tr>
-                <th><?= ucfirst($field['fieldName']) ?> :</th>
-                <td>{{ <?= $helper->getEntityFieldPrintCode($entity_twig_var_singular, $field) ?> }}</td>
-            </tr>
-<?php endforeach; ?>
-        </tbody>
-    </table>
+    <section>
+        <table class="table table-show">
+            <tbody>
+    <?php foreach ($entity_fields as $field): ?>
+                <tr>
+                    <th><?= ucfirst($field['fieldName']) ?> :</th>
+                    <td>{{ <?= $helper->getEntityFieldPrintCode($entity_twig_var_singular, $field) ?> }}</td>
+                </tr>
+    <?php endforeach; ?>
+            </tbody>
+        </table>
+    </section>
 {% endblock %}
