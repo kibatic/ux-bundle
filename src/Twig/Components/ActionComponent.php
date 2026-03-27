929d553a3ca061a1e74edf8911790b85f6e1080c
jcrombez <jeremy.crombez@kibatic.com>
2026-03-27 11:05:51 +0100

    improve actions

--- src/Twig/Components/ActionComponent.php
+++ src/Twig/Components/ActionComponent.php
@@ -3,7 +3,9 @@
 namespace Kibatic\UX\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\ComponentAttributes;
 use Twig\Environment;
+use Twig\Runtime\EscaperRuntime;
 
 #[AsTwigComponent(name: 'action')]
 class ActionComponent
@@ -16,6 +18,8 @@ class ActionComponent
 
     // Button
     public bool|string $label = 'Action';
+    public ?string $btnType = 'primary';
+    public ?string $btnSize = null;
     public ?string $btnClass = null;
     public bool|string $icon = 'bi:lightning-charge-fill';
 
@@ -26,4 +30,31 @@ class ActionComponent
     public bool $confirm = false;
     public ?string $confirmTitle = null;
     public ?string $confirmMessage = null;
+
+    public function __construct(
+        protected Environment $twig,
+    ) {
+    }
+
+    public function getBtnAttributes(): ComponentAttributes
+    {
+        $class = 'btn';
+
+        if ($this->btnType) {
+            $class .= " btn-{$this->btnType}";
+        }
+
+        if ($this->btnSize) {
+            $class .= " btn-{$this->btnSize}";
+        }
+
+        if ($this->btnClass) {
+            $class .= " {$this->btnClass}";
+        }
+
+        return new ComponentAttributes(
+            ['class' => $class],
+            $this->twig->getRuntime(EscaperRuntime::class)
+        );
+    }
 }
